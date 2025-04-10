449167db1e31ab84bbcc45c8fff39d41b26e09cc
itoshi-sae-prime <tukien159@gmail.com>
2025-04-10 13:43:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,7 @@ mongoose.connect("mongodb+srv://admin123:050825%21%40%23%24Tt@kicapdb.sxbgl.mong
     .catch(err => console.error("❌ Lỗi kết nối:", err));;
 
 app.use(bodyParse.json({ limit: "50mb" }));
-app.use(cors(
-    {  // Cho phép yêu cầu từ domain này
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-}
-));
+app.use(cors({methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']}));
 dotenv.config();
 app.use(
     bodyParse.urlencoded({
